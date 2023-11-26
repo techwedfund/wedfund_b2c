34f4b855b9826b6c9cabd23b45bed2e35fc0b73b
techwedfund <144350510+techwedfund@users.noreply.github.com>
2023-11-26 14:58:53 +0530

    initial commit

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -34,7 +34,7 @@ function Navbar () {
         <>
             <nav className="navbar navbar-expand-lg fixed-top">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/"><img src={logo} rel="noopener noreferrer" className='img-fluid logo' alt='logo' />WedFund</Link>
+                    <Link className="navbar-brand" to="https://wed.fund"><img src={logo} rel="noopener noreferrer" className='img-fluid logo' alt='logo' />WedFund</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
